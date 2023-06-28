6be74babcd327f0db59177294c1d7d29148bc06d
Jake Howard <78842458+howardjake@users.noreply.github.com>
2023-06-28 14:27:44 -0400

    Time: 62 ms (47.46%), Space: 43.4 MB (7.21%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+  if (t.includes(s)) {
+    return true
+  }
+  let tArray = Array.from(t).reverse()
+  let i = s.length - 1
+  while (i >= 0) {
+    if (tArray.indexOf(s[i]) >= 0) {
+      tArray = tArray.slice(tArray.indexOf(s[i]) + 1)
+    } else {
+      return false
+    }
+    i--
+  }
+  return true
+};
