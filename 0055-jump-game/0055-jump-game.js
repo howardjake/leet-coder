7a77ab28ea02d46c552e8d0f7f6a4cb9a04018e7
Jake Howard <78842458+howardjake@users.noreply.github.com>
2023-06-29 02:49:55 -0400

    Time: 76 ms (54.1%), Space: 46.6 MB (47.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -5,7 +5,6 @@
 var canJump = function(nums) {
     p = 0
     for (let i = 0; i < nums.length; i++) {
-        console.log(i, p)
         if (i > p) {
             return false
         } else if ( p >= nums.length) {
