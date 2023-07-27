004df8cb38629bdbbb746050b12e9944cbce871c
Jake Howard <78842458+howardjake@users.noreply.github.com>
2023-07-27 10:50:20 -0400

    Time: 47 ms (97.39%), Space: 42.2 MB (50.27%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const tmp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
+    };
+  }
+  matrix.forEach((item, idx) => item.reverse())
+};
