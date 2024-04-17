986b9b351e4c329ec1e7eb3a94ff1d24bfeee4e7
Jake Howard <78842458+howardjake@users.noreply.github.com>
2024-04-17 16:23:12 -0400

    Time: 93 ms (6.9%), Space: 53.8 MB (5.62%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    const sorted = citations.sort((a, b) => b - a)
+    let i = 0
+
+    while (i < citations[i]) {
+        console.log(i)
+        i++}
+    return i
+};
